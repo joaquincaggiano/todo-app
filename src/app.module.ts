dccd6443d59c93ddce6a715e6f37b281d403b06a
Moilab <moilab.web@gmail.com>
2026-05-19 16:45:03 +0200

    feat(): hello world resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { join } from 'path';
       // playground: false,
       // debug: false,
     }),
+    HelloWorldModule,
   ],
   controllers: [],
   providers: [],
