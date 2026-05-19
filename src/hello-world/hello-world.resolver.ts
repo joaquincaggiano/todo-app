154f77804986fc0eefdd4dc7ca49f2ab436749ad
Moilab <moilab.web@gmail.com>
2026-05-19 17:41:17 +0200

    feat(): new query randomNumber

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, { name: 'hello', description: 'Hello World Message' })
   helloWorld(): string {
     return 'Hello World';
+  }
+
+  @Query(() => Float, { name: 'randoNumber', description: 'Random Number' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
   }
 }
