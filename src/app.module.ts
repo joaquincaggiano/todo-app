fc2f06bb9e77cd628ec8a111a30618a050f47771
Moilab <moilab.web@gmail.com>
2026-05-19 17:31:03 +0200

    feat(): apollo server playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      // playground: false,
-      // debug: false,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     HelloWorldModule,
   ],
